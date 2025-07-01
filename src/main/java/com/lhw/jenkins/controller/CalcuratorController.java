bb1cb83cc33c4789dd639b36fd2f99ed7cec9356
hyunwoo_lim <dla6963@naver.com>
2025-07-01 11:49:28 +0900

    'second commit'

--- src/main/java/com/lhw/jenkins/controller/CalcuratorController.java
+++ src/main/java/com/lhw/jenkins/controller/CalcuratorController.java
@@ -7,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//주석 추가
 @Slf4j
 @RestController
 @RequiredArgsConstructor
