81111a3457991ebbc54d1c4ba8626d222e1c374f
hyunwoo_lim <dla6963@naver.com>
2025-07-01 10:39:28 +0900

    firstcommit

---
+++ src/main/java/com/lhw/jenkins/Chap12JenkinsLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.lhw.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap12JenkinsLectureSourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Chap12JenkinsLectureSourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lhw/jenkins/Chap12JenkinsLectureSourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhw.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chap12JenkinsLectureSourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
