375e5e0c44e61a5977f04095c81317c6c96815d0
hyunwoo_lim <dla6963@naver.com>
2025-07-02 10:19:43 +0900

    'Discord 웹훅 연동 테스트 - 2'

--- src/test/java/com/lhw/jenkins/service/CalculatorServiceTest.java
+++ src/test/java/com/lhw/jenkins/service/CalculatorServiceTest.java
@@ -7,7 +7,6 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
